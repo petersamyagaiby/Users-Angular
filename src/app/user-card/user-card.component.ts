f69bd1a5cfc7e7931f3476a0d94fb6cf8b23db1f
petersamyagaiby <petersamyagaiby@gmail.com>
2024-08-08 12:05:13 +0300

    Using date pipe for users birthdate

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,10 +1,10 @@
-import { NgStyle } from '@angular/common';
+import { DatePipe, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, DatePipe],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css',
 })
